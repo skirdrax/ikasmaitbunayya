b55bf20216002f75f5d038f34f922266760020c2
sakhiard <ardrasakhi390@gmail.com>
2026-08-01 22:15:48 +0700

    ok 5

--- src/components/LogoMarquee.jsx
+++ src/components/LogoMarquee.jsx
@@ -1,44 +1,124 @@
 export default function LogoMarquee() {
   const kampusList = [
-    { nama: 'Universitas Padjadjaran', logo: '/assets/kampus/UNPAD.png' },
-    { nama: 'Institut Pertanian Bogor', logo: '/assets/kampus/IPB.png' },
-    { nama: 'Universitas Brawijaya', logo: '/assets/kampus/UB.png' },
+    {
+      nama: 'Universitas Padjadjaran',
+      logo: '/assets/kampus/UNPAD.png',
+    },
+    {
+      nama: 'Institut pertanian Bogor',
+      logo: '/assets/kampus/IPB.png',
+    },
+    {
+      nama: 'Universitas Brawijaya',
+      logo: '/assets/kampus/UB.png',
+    },
+
     {
       nama: 'Universitas Negeri Sebelas Maret',
       logo: '/assets/kampus/UNS.png',
     },
-    { nama: 'Universitas Negeri Jakarta', logo: '/assets/kampus/UNJ.png' },
+
+    {
+      nama: 'Universitas Negeri Jakarta',
+      logo: '/assets/kampus/UNJ.png',
+    },
+    {
+      nama: 'Universitas Negeri Semarang',
+      logo: '/assets/kampus/UNNES.png',
+    },
     {
       nama: 'Universitas Sultan Ageng Tirtayasa',
       logo: '/assets/kampus/UNTIRTA.png',
     },
     {
-      nama: 'Universitas Jenderal Soedirman',
+      nama: 'Universitas jendral Soedirman',
       logo: '/assets/kampus/UNSOED.png',
     },
-    { nama: 'Universitas Siliwangi', logo: '/assets/kampus/UNSIL.png' },
-    { nama: 'Universitas Negeri Lampung', logo: '/assets/kampus/UNILA.png' },
-    { nama: 'Institut Seni Budaya Bandung', logo: '/assets/kampus/ISBI.png' },
-    { nama: 'UIN Jakarta', logo: '/assets/kampus/UINJ.png' },
-    { nama: 'UIN Bandung', logo: '/assets/kampus/UINB.png' },
-    { nama: 'UIN Purwokerto', logo: '/assets/kampus/UINP.png' },
+    {
+      nama: 'Universitas Siliwangi',
+      logo: '/assets/kampus/UNSIL.png',
+    },
+
+    {
+      nama: 'Universitas Negeri Lampung',
+      logo: '/assets/kampus/UNILA.png',
+    },
+    {
+      nama: 'Institut Seni Budaya Bandung',
+      logo: '/assets/kampus/ISBI.png',
+    },
+    {
+      nama: 'UIN Jakarta',
+      logo: '/assets/kampus/UINJ.png',
+    },
+    {
+      nama: 'UIN Bandung',
+      logo: '/assets/kampus/UINB.png',
+    },
+    {
+      nama: 'UIN Purwokerto',
+      logo: '/assets/kampus/UINP.png',
+    },
+    {
+      nama: 'UIN Salatiga',
+      logo: '/assets/kampus/UINS.png',
+    },
+
     {
       nama: 'Politeknik Negeri Indramayu',
       logo: '/assets/kampus/POLINDRA.png',
     },
-    { nama: 'Politeknik Negeri Batam', logo: '/assets/kampus/POLIBATAM.png' },
+    {
+      nama: 'Politeknik Negeri Batam',
+      logo: '/assets/kampus/POLIBATAM.png',
+    },
     {
       nama: 'Politeknik Negeri Media Kreatif',
       logo: '/assets/kampus/POLIMEDIA.png',
     },
-    { nama: 'Politeknik Negeri Lampung', logo: '/assets/kampus/POLINELA.png' },
+    {
+      nama: 'Politeknik Negeri Lampung',
+      logo: '/assets/kampus/POLINELA.png',
+    },
 
-    { nama: 'Universitas Pertamina', logo: '/assets/kampus/UPER.png' },
-    { nama: 'Telkom University', logo: '/assets/kampus/TELKOM.png' },
-    { nama: 'Universitas Pancasila', logo: '/assets/kampus/UP.png' },
-    { nama: 'Universitas Esa Unggul', logo: '/assets/kampus/EU.png' },
-    { nama: 'Global Institute University', logo: '/assets/kampus/GLO.png' },
-    { nama: 'Politeknik Kirana', logo: '/assets/kampus/PK.png' },
+    {
+      nama: 'Politeknik Negeri Banyuwangi',
+      logo: '/assets/kampus/POLIWANGI.png',
+    },
+
+    {
+      nama: 'Universitas Pertamina',
+      logo: '/assets/kampus/UPER.png',
+    },
+    {
+      nama: 'Telkom University',
+      logo: '/assets/kampus/TELKOM.png',
+    },
+    {
+      nama: 'Universitas Pancasila',
+      logo: '/assets/kampus/UP.png',
+    },
+    {
+      nama: 'Universitas Ahmad Dahlan',
+      logo: '/assets/kampus/UAD.png',
+    },
+
+    {
+      nama: 'Universitas Esa Unggul',
+      logo: '/assets/kampus/EU.png',
+    },
+    {
+      nama: 'Global Institute University',
+      logo: '/assets/kampus/GLO.png',
+    },
+    {
+      nama: 'Universitas Terbuka',
+      logo: '/assets/kampus/UT.png',
+    },
+    {
+      nama: 'Politeknik Kirana',
+      logo: '/assets/kampus/PK.png',
+    },
   ];
 
   // Duplikat 3x buat infinite

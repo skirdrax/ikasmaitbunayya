ea2f6494418d855bc989a4095380bb1d600abfbd
sakhiard <ardrasakhi390@gmail.com>
2026-08-02 00:58:02 +0700

    ok 8

--- src/components/VisitorCounter.jsx
+++ src/components/VisitorCounter.jsx
@@ -1,29 +1,36 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 export default function VisitorCounter() {
-  const containerRef = useRef(null);
-
   useEffect(() => {
-    if (containerRef.current) {
-      // Kosongkan container
-      containerRef.current.innerHTML = '';
+    // Script auth
+    const script1 = document.createElement('script');
+    script1.src =
+      'https://www.freevisitorcounters.com/auth.php?id=6e36f5cfb3c336b6a85e11e2ff08c5ec9d236822';
+    script1.async = true;
 
-      // SCRIPT API PERTAMA (ID: 1609833)
-      const script1 = document.createElement('script');
-      script1.src =
-        'https://www.freevisitorcounters.com/auth.php?id=3d2a46c5f887c80ce0d58517e842a3f868ab831c';
-      script1.async = true;
+    // Script counter
+    const script2 = document.createElement('script');
+    script2.src =
+      'https://www.freevisitorcounters.com/en/home/counter/1609836/t/0';
+    script2.async = true;
 
-      // SCRIPT API KEDUA (ID: 1609833)
-      const script2 = document.createElement('script');
-      script2.src =
-        'https://www.freevisitorcounters.com/en/home/counter/1609833/t/0';
-      script2.async = true;
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
 
-      containerRef.current.appendChild(script1);
-      containerRef.current.appendChild(script2);
-    }
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
   }, []);
 
-  return <div ref={containerRef} className="visitor-counter" />;
+  return (
+    <div>
+      <a
+        href="https://www.counters-free.net/"
+        target="_blank"
+        rel="noopener noreferrer">
+        Free Counters
+      </a>
+    </div>
+  );
 }

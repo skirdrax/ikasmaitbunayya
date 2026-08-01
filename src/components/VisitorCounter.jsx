9d900ec74861036e27af81319d4d23716a986d35
sakhiard <ardrasakhi390@gmail.com>
2026-08-02 01:26:17 +0700

    ok 9

--- src/components/VisitorCounter.jsx
+++ src/components/VisitorCounter.jsx
@@ -15,21 +15,6 @@ export default function VisitorCounter() {
           />
         </a>
       </div>
-
-      {/* ===== VIEW DAILY ===== */}
-      <div className="visitor-item">
-        <span className="visitor-label">Hari Ini</span>
-        <a
-          href="https://visitorbadge.io/status?path=ika-smaitbunayya.web.id"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="visitor-badge-link">
-          <img
-            src="https://api.visitorbadge.io/api/daily?path=ika-smaitbunayya.web.id&label=View%20Daily&labelColor=%23697689&countColor=%23263759&style=plastic&labelStyle=none"
-            alt="View Daily"
-          />
-        </a>
-      </div>
     </div>
   );
 }
